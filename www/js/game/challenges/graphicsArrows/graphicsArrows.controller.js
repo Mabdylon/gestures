f428dfdb2c6cd5fe88eec0b501e7a1ea4a757ac3
Mabdylon <mathieu.barat@gmail.com>
2015-03-25 21:39:06 +0100

    Ajout ionics icons et quelques refonte graphique, debut travail animation

--- www/js/game/challenges/graphicsArrows/graphicsArrows.controller.js
+++ www/js/game/challenges/graphicsArrows/graphicsArrows.controller.js
@@ -1,46 +1,60 @@
 gesturesApp.controller('game.challenges.graphicsArrows.graphicsArrows.controller', ['$stateParams', '$ionicGesture', 'game.modes.modes.service',
     function($stateParams, $ionicGesture, modesService) {
 
-        var challengeContentElement = angular.element(document.querySelector( '#challenge-content-element' ) );
-        
+        var self = this;
+        var challengeContentElement = angular.element(document.querySelector('#challenge-content-element'));
+        var graphicArrowElement = angular.element(document.querySelector('#graphic-arrow'));
+
         var success = function(e) {
-            if(e.gesture.distance > 150) {
+            console.log(e);
+            window.requestAnimationFrame(function() {
+                self._doDrag(e);
+                return 24;
+            });
+            if (e.gesture.distance > 150) {
+                $ionicGesture.off(this.currentAction);
                 modesService.success();
             }
+        };
+
+        this._doDrag = function(e) {
+            graphicArrowElement[0].style[ionic.CSS.TRANSFORM] = 'translate3d(' + e.gesture.deltaX * 3 + 'px, ' + e.gesture.deltaY * 3 + 'px, 0)';
         };
 
         var values = {
             up: {
                 text: 'Up',
+                icon: 'ion-android-arrow-up',
                 action: function() {
                     return $ionicGesture.on('dragup', success, challengeContentElement);
                 }
             },
             down: {
                 text: 'Down',
+                icon: 'ion-android-arrow-down',
                 action: function() {
                     return $ionicGesture.on('dragdown', success, challengeContentElement);
                 }
             },
             left: {
                 text: 'Left',
+                icon: 'ion-android-arrow-left',
                 action: function() {
                     return $ionicGesture.on('dragleft', success, challengeContentElement);
                 }
             },
             right: {
                 text: 'Right',
+                icon: 'ion-android-arrow-right',
                 action: function() {
                     return $ionicGesture.on('dragright', success, challengeContentElement);
                 }
             }
         };
-        
+
         var challenge = values[$stateParams.challengeValue];
 
-        this.graphicsArrows = {
-            text: challenge.text,
-            action: challenge.action()
-        };
+        this.graphicsArrows = challenge;
+        this.currentAction = this.graphicsArrows.action();
 
-}]);
+    }]);
--- www/js/game/modes/modes.services.js
+++ www/js/game/modes/modes.services.js
@@ -3,14 +3,23 @@ gesturesApp.factory('game.modes.modes.service', function() {
     var modes = {
         list: [{
                 name: 'Speed Attack',
-                state: 'speedAttack'
+                state: 'speedAttack',
+                button: {
+                    color: 'button-positive'
+                },
+                bar: {
+                    color: 'bar-positive'
+                },
+                background: {
+                    color: 'positive'
+                }
             }],
         currentMode: null
     };
 
     return {
         addToList: function(mode) {
-          modes.list.push(mode);
+            modes.list.push(mode);
         },
         getList: function() {
             return modes.list;
